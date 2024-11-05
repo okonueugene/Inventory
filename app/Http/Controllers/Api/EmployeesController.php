92485ebfc29a74b1c6556fe7858c4ac71a7191e4
okonueugene <tylarrex67@gmail.com>
2024-11-05 11:56:21 +0300

    feat:added seeders and more methods

--- app/Http/Controllers/Api/EmployeesController.php
+++ app/Http/Controllers/Api/EmployeesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class EmployeesController extends Controller
 {
