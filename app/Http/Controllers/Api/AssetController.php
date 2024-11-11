0a54b28e3c077b54c829a997f35edf8dbe1e186e
okonueugene <tylarrex67@gmail.com>
2024-11-11 12:13:45 +0300

    fix:fetch using code

--- app/Http/Controllers/Api/AssetController.php
+++ app/Http/Controllers/Api/AssetController.php
@@ -87,7 +87,7 @@ class AssetController extends Controller
 
         //check if image is uploaded
         if ($request->hasFile('image')) {
-            return response()->json($request->file('image'));
+            dd($request->file('image'));
             $asset->addMediaFromRequest('image')->toMediaCollection('asset_images');
         }
 
